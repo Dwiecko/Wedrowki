390644315784d000ded18cbbd8d59229b23c6fe8
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-04-15 22:09:01 +0200

    User ctor

--- ServerApp/Models/User.cs
+++ ServerApp/Models/User.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Security;
+
 namespace ServerApp.Models
 {
     public class User
     {
-       public string Name { get; private set; }
-       public string Email { get; private set; }
-       public string Password { get; private set; }
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string name, string email, string password)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Email = email;
+            Password = password;
+        }
     }
 }
