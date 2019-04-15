6c40084cc5cac44d9744d756a9bcad23fd453bb3
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-04-15 22:03:12 +0200

    Coord class

---
+++ ServerApp/Models/Coord.cs
@@ -0,0 +1,11 @@
+public struct Coord
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Coord(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
