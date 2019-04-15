cffd1fd05e0df5f0aebecd43d51c0b3f008cad84
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-04-15 21:57:44 +0200

    User model

---
+++ ServerApp/Models/User.cs
@@ -0,0 +1,9 @@
+namespace ServerApp.Models
+{
+    public class User
+    {
+       public string Name { get; private set; }
+       public string Email { get; private set; }
+       public string Password { get; private set; }
+    }
+}
