5707818d92b32094ce72c4805596f87d5c35d74e
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-09-11 18:37:38 +0200

    Remove struct initializer

--- ServerApp/Models/Coord.cs
+++ ServerApp/Models/Coord.cs
@@ -1,11 +1,6 @@
 public struct Coord
 {
-    public int X { get; private set; }
-    public int Y { get; private set; }
+    public int X { get; set; }
 
-    public Coord(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
+    public int Y { get; set; }
 }
